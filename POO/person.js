2276fae6b85d02c8b9fa8b430bcb32da42513758
inassinf <inass.infantes@gmail.com>
2023-02-15 15:53:28 +0100

    Reto finalizado

--- POO/person.js
+++ POO/person.js
@@ -5,27 +5,24 @@
         this.altura = hight
         this.peso = weight
         this.nacionalidad
-        this.anioActual
         this.yearOfBirth
     }
 
-    imc(){this.peso/(this.altura * this.altura) }
+    imc(){return Math.floor(this.peso/(this.altura * this.altura)) }
 
-    edad() {(anioActual - Jose.yearOfBirth) }
+    edad(anioActual) {return (anioActual - this.yearOfBirth) }
 
-    printAll(){console.log(Persona);}
 
     printAll(){
         for(let i=0; i<this.hobbies.lenght; i++){
-            console.log(this.hobbies[i]); }
-    }}
+            console.log("Sus hobbies son: " + this.hobbies[i]); }
+      console.log("su color de ojos es: " + this.colorOjos);
+      console.log("su altura es: " + this.altura + " m");
+      console.log( "su peso es : " + this.peso + " kg" );
+      console.log("su nacionalidad es: " + this.nacionalidad);
 
-let Jose = new Persona(1.7, 80);
-Jose.yearOfBirth = 1955;
-Jose.anioActual = 2023;
-Jose.hobbies= ["andar", "viajar", "comer", "correr"]
-console.log(Jose.imc);
-console.log("Su edad es" + edad() + "anios");
-Jose.printAll()
+    }
+
+ }
 module.exports = {Persona};
 
--- POO/personTest.js
+++ POO/personTest.js
@@ -1,4 +1,20 @@
-let myLib = require("Persona") 
+let {Persona} = require ("./person") 
 
 let Nur = new Persona(1.60, 60);
-console.log("Nuestra persona es: " + Nur);
+// console.log("Nuestra persona es: " + Nur);
+
+let Jose = new Persona(1.7, 80);
+Jose.colorOjos ="azul";
+Jose.yearOfBirth = 1955;
+Jose.anioActual = 2023;
+Jose.nacionalidad = "portuguesa"
+Jose.hobbies= ["andar", "viajar", "comer", "correr"]
+
+// console.log(Jose.imc);
+// console.log("Su edad es" + edad() + "anios");
+// Jose.printAll()
+
+console.log("Su IMC es: " + Jose.imc()); 
+
+Jose.printAll();
+console.log("Tiene " + Jose.edad(2023) + " anios");
